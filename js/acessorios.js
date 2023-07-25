b929fdc3ccb848331fa628654c5ad77dd6d0e2c2
Ricosan49 <73192661+Ricosan49@users.noreply.github.com>
2023-07-25 14:22:33 -0300

    Add files via upload

--- js/acessorios.js
+++ js/acessorios.js
@@ -78,9 +78,22 @@ function acessolaser() {
     list.innerHTML = ''
     mod.innerHTML = 'Acessorios LASER<br>'
     divptitulo.innerHTML = ''  
-    var ler0 = '<a   class="advera" >WBZ1024025 - ENCODER LASER: ENCODER 2000 PPR EIXO 6MM SAIDA A  B CABO 4 M</a><br>'
-    var ler1 = '<a   class="advera" >WBZ0042044 - SENSOR REFLEXAO DIFUSA PNP (STANDARD)</a><br>'
-    var ler2 = '<a   class="advera" >WBZ0042035 - REFLETOR BOS-R-1 (espelho)</a><br>'
-    list.innerHTML = `${ler0} ${ler1} ${ler2}` 
+	var ler0 = '<a   class="advera" >WBZ0042044 - SENSOR REFLEXAO DIFUSA PNP (STANDARD)</a><br>'
+    var ler1 = '<a   class="advera" >WBZ0042035 - REFLETOR BOS-R-1 (espelho)</a><br>'
+    var ler2 = '<a class="advera" >WBZ1024025 - ENCODER LASER: ENCODER 2000 PPR EIXO 6MM SAIDA A  B CABO 4 M</a><br>'
+	var ler3 = '<a calss="advera" >WBZ1024026 - SUPORTE UNIVERSAL DE ENCODER</a><br>'
+	var ler4 = '<a href="acessorios/encoder e suporte.pdf"  class="advera" >Imagem do Encoder/Suporte</a><br>'
+	var ler5 = '<a   class="advera" >WBZ9999008 - Alarme Luminoso e Sonoro (nacional)</a><br>'
+	var ler6 = '<a href="acessorios/Alarme Luminoso Nacional.pdf"  class="advera" >Imagem/BT do Alarme</a><br>'
+	var ler7 = '<a   class="advera" >WBZ0003142 - Caixa Filtro pre extrator</a><br>'
+	var ler8 = '<a href="acessorios/cxpreestrator.PDF"  class="advera" >Imagem/BT da Caixa</a><br>'
+	var ler9 = '<a   class="advera" >AL-SP93444 - Medidor Potencia LASER CO2</a><br>'
+	var ler10 = '<a href="acessorios/MedidorCO2.pdf"  class="advera" >Imagem do Medidor CO2</a><br>'
+	var ler11 = '<a   class="advera" >AL-SP67438 - Medidor Potencia LASER FIBRA / YAG / CO2</a><br>'
+	var ler12 = '<a href="acessorios/MedidorCO2FibraYag.pdf"  class="advera" >Imagem do Medidor CO2</a><br>'
+	var ler13 = '<a   class="advera" >WBZ0003024 - Suporte Padrão Laser</a><br>'
+	var ler14 = '<a href="acessorios/SUPORTE PADRÃO VIDEOJET.PDF"  class="advera" >Suporte Padrão Laser</a><br>'
+    list.innerHTML = `${ler0} ${ler1} ${ler2} ${ler3} ${ler4} ${ler5} ${ler6} ${ler7} ${ler8} ${ler9} ${ler10} ${ler11}
+	${ler12} ${ler13} ${ler14}` 
     
 }
