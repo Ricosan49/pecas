c7f95a0bdfa300306c5c261daa6e64e9d94e357a
Ricosan49 <73192661+Ricosan49@users.noreply.github.com>
2024-02-29 15:32:08 -0300

    Add files via upload

---
+++ js/telasmaq.js
@@ -0,0 +1,25 @@
+var divres = window.document.getElementById('ver')
+var divptitulo = window.document.getElementById('divpti')
+var mod = window.document.getElementById('modelo')
+var list = window.document.getElementById('lista')
+
+function telamaq() {
+	divres.innerHTML = ''
+    list.innerHTML = ''
+    mod.innerHTML = ''
+    divptitulo.innerHTML = 'Telas de Maquinas</br>'
+	var acesso1 = `<button  onclick='abrir("1040")' class="botaodiv">Tela 1040</button></br>`
+	var acesso2 = `<button  onclick='abrir("tto")' class="botaodiv">Tela TTO</button></br>`
+	var acesso3 = `<button  onclick='abrir("tes")' class="botaodiv">teste</button></br>`
+	list.innerHTML = `<p class="adivera"></p>${acesso1} ${acesso2} ${acesso3}`
+}
+function abrir(tel) {
+	var telas = tel
+	if (telas == "1040") {
+		window.open('tela1040/telaprincipal.html')
+	}else if (telas == "tto") {
+		window.open('telatto/indextto.html')
+	}else if(telas == "tes") {
+		list.innerHTML(windows.open('telatto/indextto.html'))
+	}else {alert('ERRO!!! este item não existe')}
+}
